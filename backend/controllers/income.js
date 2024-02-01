0a033b7f765b1de8cffd46878e97286b8c494a4f
Marketing Director <nicolestark1103@gmail.com>
2024-02-01 13:39:06 -0600

    working on backend

---
+++ backend/controllers/income.js
@@ -0,0 +1,7 @@
+const IncomeSchema = require("../models/IncomeModel");
+
+exports.addIncome = async (req, res) => {
+  const { title, amount, category, description, date } = req.body;
+
+  const income = IncomeSchema;
+};
